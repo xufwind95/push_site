06bdef002e3266ab7c442a1092f82659aaa0ff74
xufwind95 <xuf95@163.com>
2019-02-21 14:57:30 +0800

    new branch test uwsgi

--- push_site/settings.py
+++ push_site/settings.py
@@ -76,9 +76,18 @@ WSGI_APPLICATION = 'push_site.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_push',
+        'USER': 'root',  # 'cpxnormal'
+        'HOST': '127.0.0.1',
+        'PASSWORD': '123456',  # 'cpx_normal_098'
+        'PORT': 3306,
     }
 }
 
